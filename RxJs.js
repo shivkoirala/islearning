88981cca142f188b10153638493b490a8c0e06ae
shivkoirala <shiv_koirala@yahoo.com>
2019-01-17 13:47:33 +0530

    Update RxJs.js

--- RxJs.js
+++ RxJs.js
@@ -1,14 +1,22 @@
 var Rx = require('rxjs/Rx');
+
 function Emit(observer){
     var x = 0;
     x=5;
     observer.next(x);
-    x=6;
+    x=16;
     observer.next(x);
     x=11;
     observer.next(x);
+    observer.error("bad");
+    observer.complete();
+
 }
-var myObservable = Rx.Observable.create(observer => Emit(observer));
-  myObservable.subscribe({
-    next: x => console.log('got value ' + x)
+var myObservable = new  Rx.Observable(Emit);
+  myObservable.
+    filter(x=>x>10)
+    .subscribe({
+    next: x => console.log('got value ' + x),
+    complete: () => console.log('Completed'),
+    error:e=>console.log(e)
   });
